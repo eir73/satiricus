6ff31efe19f3abd8cf372b90b3803be838db8bdc
eir73 <roflaneir73@gmail.com>
2020-09-17 19:35:11 +0300

    adaptive sliders for detail page

--- js/detail-swiper.js
+++ js/detail-swiper.js
@@ -1,9 +1,40 @@
 const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 4,
-    loop: true,
-    spaceBetween: 30,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+  slidesPerView: 4,
+  loop: true,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    200: {
+      slidesPerView: 'auto'
     },
+    575: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 30
+    }
+  }
+});
+
+
+if (window.innerWidth < 660) {
+  const btn = document.querySelector('.seats-buy-btn')
+  document.querySelector('.seats-map').classList.add('swiper-wrapper')
+  const SeatsSwiper = new Swiper('.seats-map-wr', {
+    slidesPerView: 'auto',
+    freeMode: true,
   });
+}
+
