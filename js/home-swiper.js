280fc3e689ce2d31a92499dcd65fb36d86beb8f6
eir73 <roflaneir73@gmail.com>
2020-09-20 21:02:30 +0300

    home page slider pagination

--- js/home-swiper.js
+++ js/home-swiper.js
@@ -11,6 +11,16 @@ const swiper = new Swiper('.swiper-container', {
   },
 });
 
+const page1 = document.getElementById('page-1')
+const page2 = document.getElementById('page-2')
+const page3 = document.getElementById('page-3')
+
+swiper.on('slideChange', () => {
+  page1.innerHTML = '00'.concat(swiper.realIndex + 1).slice(-2)
+  page2.innerHTML = '00'.concat((swiper.realIndex + 1) % (swiper.slides.length - 2) + 1).slice(-2)
+  page3.innerHTML = '00'.concat((swiper.realIndex + 2) % (swiper.slides.length - 2) + 1).slice(-2)
+})
+
 if (window.innerWidth < 1200) {
   const teamSwiper = new Swiper('.sl-team-wr', {
     slideClass: 'team-item',
