c7e29734de56944ef22d743aa7f377910278a45f
eir73 <roflaneir73@gmail.com>
2020-09-20 12:33:27 +0300

    finish home page

--- js/home-swiper.js
+++ js/home-swiper.js
@@ -10,10 +10,35 @@ const swiper = new Swiper('.swiper-container', {
       prevEl: '.swiper-button-prev',
     },
   });
-/*
-const teamSwiper = new Swiper('.sl-team-wr', {
-  pagination: {
-    el: '.t-swiper-pagination'
-  },
-  slidesPerView: 4
-})*/
+
+if(window.innerWidth < 1200) { 
+  const teamSwiper = new Swiper('.sl-team-wr', {
+    slideClass: 'team-item',
+    pagination: {
+      el: '.t-swiper-pagination'
+    },
+    slidesPerView: 4,
+    spaceBetween: 15,
+    breakpoints: {
+      // when window width is >= 200px
+      320: {
+        slidesPerView: 'auto',
+        spaceBetween: 15
+      },
+      575: {
+        slidesPerView: 2,
+        spaceBetween: 15
+      },
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 15
+      },
+      // when window width is >= 992px
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 15
+      }
+    }
+  })
+}
